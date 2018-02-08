f7c4a3c4790fb6729872149fc612d37587e85820
xuchdeid <xuchdeid@gmail.com>
2018-02-08 19:33:45 +0800

    NEW: loading animation

--- node/eastmoney.js
+++ node/eastmoney.js
@@ -1,3 +1,4 @@
+require('http').globalAgent.maxSockets = 10;
 const request = require('request');
 
 function buildGetRequest(url, params) {
@@ -37,7 +38,7 @@ function getDataById(id) {
     return _get(url, params);
 }
 
-function getByList(tips, fcodes, process) {
+function getByList(fcodes, process, finish) {
     Promise
         .resolve(fcodes)
         .then(list => {
@@ -48,10 +49,19 @@ function getByList(tips, fcodes, process) {
             return Promise.all(all);
         })
         .then(results => {
-            console.log(tips)
+            return new Promise((resolve, reject) => {
+                setTimeout(function () {
+                    resolve(results);
+                }, 2000);
+            });
+        })
+        .then(results => {
+            if (finish) {
+                finish();
+            }
             results.map(value => {
                 process(value);
-            })
+            });
         })
         .catch(error => {
             console.log(error.message);
@@ -82,6 +92,7 @@ function showTodayValues(value) {
 
     if (Datas && Datas.length > 0) {
         //console.log(Datas);
+        printRedConsole(10, Expansion.SHORTNAME, "(-)");
         let process_datas = [];
         let max, min;
         Datas.map(data => {
@@ -100,31 +111,72 @@ function showTodayValues(value) {
         let length = max - min;
         let result = [];
         process_datas.map(data => {
-            result.push((data - min)/length);
+            result.push((data - min) / length);
         });
-        
-        let start = (0 - min)/length;
+
+        let y = ''.padEnd(100, '-') + '> y';
+        console.log(y);
+        let start = (0 - min) / length;
         result.map(value => {
-            drowLine(200, value, start);
+            drawLine(100, value, start);
         })
+        let x = ''.padEnd(parseInt((100 * start).toFixed(0)), ' ') + ' ';
+        let _x = ''.padEnd(parseInt((100 * start).toFixed(0)), ' ') + 't';
+        console.log(x);
+        console.log(_x);
     } else {
         printRedConsole(10, Expansion.SHORTNAME, "(---)");
     }
 }
 
-function drowLine(max, value, start) {
+function Canvas(width, height) {
+    this._canvas = new Array(height);
+    for (let i = 0; i < height; i++) {
+        this._canvas.push(''.padEnd(width));
+    }
+}
+
+Canvas.prototype.clean = function () {
+    this._canvas.map(line => {
+        line = ''.padEnd(width);
+    });
+}
+
+Canvas.prototype.draw = function () {
+    this._canvas.map(line => {
+        console.log(line);
+    });
+}
+
+function drawLine(max, value, start) {
     let _value = parseInt((max * value).toFixed(0));
     let _start = parseInt((max * start).toFixed(0));
+
     let line = '';
     for (let i = 0; i < max; i++) {
+        //if (i == _start) {
+        //    line += '|';
+        //} else if (i == _value) {
+        //    line += '-';
+        //} else {
+        //    line += ' ';
+        //}
         if (i == _start) {
             line += '|';
-        } else if (i <= _value) {
-            line += '.';
+        } else if (((_value < _start) && (i >= _value && i < _start)) ||
+            ((_value > _start) && (i > _start && i <= _value))) {
+            line += '-';
         } else {
             line += ' ';
         }
     }
+
+    /*if (_value >= _start) {
+        console.log('\x1b[31m%s\x1b[0m', line);
+    } else {
+        console.log('\x1b[32m%s\x1b[0m', line);
+    }*/
+
     console.log(line);
 }
 
@@ -160,11 +212,26 @@ function printGreenConsole(size, name, value) {
     console.log('\x1b[36m%s \x1b[32m%s\x1b[0m', name, _value_string);
 }
 
+var loading = function () {
+    var h = ['|', '/', '-', '\\'];
+    var i = 0;
+
+    return setInterval(() => {
+        i = (i > 3) ? 0 : i;
+        console.clear();
+        console.log(h[i]);
+        i++;
+    }, 300);
+}
+
 //var fcodes = ['519696', '001630', '000962', '001878', '004343', '501301', '210009'];
-//getByList("=========================================", fcodes, showLastValue);
+//getByList(fcodes, showLastValue);
+var id = loading();
+var fcodes = ['161715', '241001', '001542', '501301', '004346', '000962', '420003', '001810', '160716', '001878', '164906', '210009', '519696', '378006', '162415', '000961', '378546', '001549', '519983', '377016', '002400', '001559', '002086'];
+getByList(fcodes, showLastValue, function () {
+    console.clear();
+    clearInterval(id);
+});
 
-//var fcodes = ['161715', '241001', '001542', '501301', '004346', '000962', '420003', '001810', '160716', '001878', '164906', '210009', '519696', '378006', '162415', '000961', '378546', '001549', '519983', '377016', '002400', '001559', '002086'];
-//getByList("*****************************************", fcodes, showLastValue);
-
-var fcode = ['210009'];
-getByList("[--------------------------]", fcode, showTodayValues);
+//var fcode = ['519696', '001630', '000962', '001878', '004343', '501301', '210009'];
+//getByList(fcode, showLastValue);
